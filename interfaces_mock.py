4a56e2441dbbe5fba03494610c7eb0e0a6e303b1
dhelms33 <derrickhelms10@gmail.com>
2026-05-31 10:58:23 -0600

    Added new interfaces

--- interfaces_mock.py
+++ interfaces_mock.py
@@ -1,8 +1,17 @@
 from abc import ABC, abstractmethod
 
 class SocialMediaParser(ABC):
-    """ Abstract Base Class enforcing the Open Closed Principle, gathering Social Media data to be fed into model"""
+    """ Abstract Base Class enforcing the Open Closed Principle, filtering data out of the CSV"""
     
     @abstractmethod
     def load_data(self, file_path: str) -> None:
         pass
+    
+    @abstractmethod
+    def extract_engagement_metics(self) -> dict:
+        pass
+    
+    class PredicitveModel(ABC):
+        """ """
+        def train_data(self):
+            pass
